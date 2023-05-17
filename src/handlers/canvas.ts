a0ee8e8bc62a681dcca9e5a51862dead113adb1f
Mohacyr Rojas <mohacyr.rojas.contractor@bbva.com>
2023-05-17 17:47:52 -0600

    feat(canvas): pass prisma errors to express

--- src/handlers/canvas.ts
+++ src/handlers/canvas.ts
@@ -1,70 +1,108 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import prisma from "../db";
 
-export const createCanvas = async (req: Request, res: Response) => {
+export const createCanvas = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   const { name, portfolioId } = req.body;
   const userId = req.user.id;
+  try {
+    const canvas = await prisma.canvas.create({
+      data: {
+        name,
+        portfolioId,
+        userId,
+      },
+    });
 
-  const canvas = await prisma.canvas.create({
-    data: {
-      name,
-      portfolioId,
-      userId,
-    },
-  });
-
-  res.json({ data: canvas });
+    res.json({ data: canvas });
+  } catch (error) {
+    next(error);
+  }
 };
 
-export const getCanvas = async (req: Request, res: Response) => {
+export const getCanvas = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   const { id } = req.params;
 
-  const canvas = await prisma.canvas.findUnique({
-    where: {
-      id: id,
-    },
-  });
+  try {
+    const canvas = await prisma.canvas.findUnique({
+      where: {
+        id: id,
+      },
+    });
 
-  res.json({ data: canvas });
+    res.json({ data: canvas });
+  } catch (error) {
+    next(error);
+  }
 };
 
-export const getAllCanvases = async (req: Request, res: Response) => {
+export const getAllCanvases = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   const userId = req.user.id;
+  try {
+    const canvases = await prisma.canvas.findMany({
+      where: {
+        userId,
+      },
+    });
 
-  const canvases = await prisma.canvas.findMany({
-    where: {
-      userId,
-    },
-  });
-
-  res.json({ data: canvases });
+    res.json({ data: canvases });
+  } catch (error) {
+    next(error);
+  }
 };
 
-export const updateCanvas = async (req: Request, res: Response) => {
+export const updateCanvas = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   const { id } = req.params;
   const { name, sketch } = req.body;
 
-  const canvas = await prisma.canvas.update({
-    where: {
-      id: id,
-    },
-    data: {
-      name,
-      sketch,
-    },
-  });
+  try {
+    const canvas = await prisma.canvas.update({
+      where: {
+        id: id,
+      },
+      data: {
+        name,
+        sketch,
+      },
+    });
 
-  res.json({ data: canvas });
+    res.json({ data: canvas });
+  } catch (error) {
+    next(error);
+  }
 };
 
-export const deleteCanvas = async (req: Request, res: Response) => {
+export const deleteCanvas = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   const { id } = req.params;
 
-  const canvas = await prisma.canvas.delete({
-    where: {
-      id: id,
-    },
-  });
+  try {
+    const canvas = await prisma.canvas.delete({
+      where: {
+        id: id,
+      },
+    });
 
-  res.json({ data: canvas });
+    res.json({ data: canvas });
+  } catch (error) {
+    next(error);
+  }
 };
